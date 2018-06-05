c996e14a5942556051b77772784fc5daec492640
Michelle Kruk <kruk.mich@gmail.com>
2018-06-05 21:40:16 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,10 +1,14 @@
-student_name = []
+#student_name = []
 #arrays are formed by using square brackets [ ] 
 
-student_names = ['sam', 'jess', 'ash', 'bob', 'carla']
-puts student_names
+#student_names = ['sam', 'jess', 'ash', 'bob', 'carla']
+#puts student_names
 
 #puts [name of your array] prints out the imformation in your array in he terminal
-puts student_names[0]
+#puts student_names[0]
 
 #position Zero 0 gives you the first value in the array
+
+
+student_names.push('cardi')
+puts student_names
